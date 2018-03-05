2a178c50aca75d093c3aff8e95869ef42ea03df1
zurachu <zurachu@gmail.com>
2018-03-05 23:19:29 +0900

    引数 color 使ってなかったので修正

--- piece/test.rb
+++ piece/test.rb
@@ -1,10 +1,10 @@
 def draw_polygon(color, p)
   i = 1
   while i < p.size do
-    lcd_line(0, p[i-1][0], p[i-1][1], p[i][0], p[i][1])
+    lcd_line(color, p[i-1][0], p[i-1][1], p[i][0], p[i][1])
     i += 1
   end
-  lcd_line(0, p[i-1][0], p[i-1][1], p[0][0], p[0][1])
+  lcd_line(color, p[i-1][0], p[i-1][1], p[0][0], p[0][1])
 end
 
 lcd_paint(3, 0, 0, 128, 88)
